ad6b9e42aa46847dfedd54b78fc13ca5e71e19da
Max N <nikolaev.makc@gmail.com>
2022-07-13 18:43:59 +0300

    license

---
+++ license.go
@@ -0,0 +1,20 @@
+package vmmanagerapi
+
+import (
+	"encoding/json"
+
+	"github.com/gadost/go-vmmanager-api/types"
+)
+
+func (a *Api) LicensePacket(params *types.License) (*types.ID, error) {
+	payload, _ := json.Marshal(params)
+	bodyResp, err := a.NewRequest(
+		payload,
+		"/packet",
+		requestTypePost,
+		DefaultService)
+
+	var t *types.ID
+	json.Unmarshal(bodyResp, &t)
+	return t, err
+}
